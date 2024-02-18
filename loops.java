cf5fe3d397daa02ee753c3b0a125db2327eee69f
Sahil Rohankar <114298849+Sahilicious@users.noreply.github.com>
2024-02-18 18:47:06 +0530

    Add files via upload

---
+++ loops.java
@@ -0,0 +1,30 @@
+public class loops {
+    public static void main (String args[] ){
+
+// For loop To print no. in order
+//        for (int counter=0; counter<11; counter++){
+//            System.out.println(counter+ " ");}
+
+// While loop to print no. in order
+//        int i =0;
+//        while(i<11){
+//            System.out.println(i);
+//            i++; } // i=i+1;
+
+// DO while loop to print no. in order
+//          int i=0;
+//          do {
+//              System.out.println(i);
+//              i=i+1;
+//          } while(i<11);
+//
+
+// Do while loop another example
+        int i=20;
+        do{
+            System.out.println("It prints the output, afterwards check the condition");
+        }while(i<10);
+
+
+    }
+}
